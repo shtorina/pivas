91e1fedc57f54d38f9b9ba635a599ccf070892e7
shtorina <havgaidea@gmail.com>
2026-08-11 20:43:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,29 +10,44 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Массив для хранения истории сообщений в памяти
+const messagesHistory = [];
+
 io.on('connection', (socket) => {
-    // По умолчанию сажаем пользователя в комнату "general"
+    // Подключаем к комнате по умолчанию
     socket.join('general');
 
-    // Переключение между комнатами
+    // Сразу отправляем всю сохраненную историю новому пользователю
+    socket.emit('chat history', messagesHistory);
+
+    // Переключение комнат
     socket.on('join room', (room) => {
-        // Выходим из всех предыдущих комнат, кроме личного ID
         for (const r of socket.rooms) {
             if (r !== socket.id) socket.leave(r);
         }
         socket.join(room);
     });
 
-    // Получение и рассылка сообщения конкретной комнате
+    // Прием и рассылка сообщений
     socket.on('chat message', (msg) => {
-        // Отправляем только людям в той же комнате
-        io.to(msg.room).emit('chat message', msg);
+        // Гарантируем, что комната указана
+        const room = msg.room || 'general';
+        const messageData = { ...msg, room };
+
+        // Сохраняем в историю (максимум 100 последних сообщений)
+        messagesHistory.push(messageData);
+        if (messagesHistory.length > 100) {
+            messagesHistory.shift();
+        }
+
+        // Отправляем всем в этой комнате
+        io.to(room).emit('chat message', messageData);
     });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-    console.log('=================================');
-    console.log('>>> Pivas Server Online [Port 3000] <<<');
-    console.log('=================================');
+    console.log('====================================');
+    console.log(>>> Pivas Server Online [Port ${PORT}] <<<);
+    console.log('====================================');
 });
